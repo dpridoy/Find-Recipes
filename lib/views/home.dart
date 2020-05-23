dfeb0ebc08be635b64fcbb6211f06ca19a949898
Nasirul Haque Ridoy <dpridoy@gmail.com>
2020-05-24 01:24:47 +0600

    final commit creating WebView

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
 import 'dart:io';
-
+import 'package:findrecipes/views/recipe_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:findrecipes/models/recipe_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class Home extends StatefulWidget {
@@ -11,6 +15,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<RecipeModel> recipes = new List<RecipeModel>();
+
+  TextEditingController textEditingController=new TextEditingController();
+
+  String applicationId="116ade3e";
+  String applicationKey="9d8bc51d52f046cba2eaada739af551e";
+
+  getRecipes(String query) async {
+    String url="https://api.edamam.com/search?q=$query&app_id=116ade3e&app_key=9d8bc51d52f046cba2eaada739af551e";
+
+    var response = await http.get(url);
+    Map<String,dynamic> jsonData= jsonDecode(response.body);
+    jsonData["hits"].forEach((element){
+//      print(element.toString());
+      RecipeModel recipeModel=new RecipeModel();
+      recipeModel = RecipeModel.fromMap(element["recipe"]);
+      recipes.add(recipeModel);
+
+    });
+    setState(() {
+
+    });
+    print("${recipes.toString()}");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,63 +58,118 @@ class _HomeState extends State<Home> {
               )
             ),
           ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: Platform.isIOS? 60: 40,horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: kIsWeb?MainAxisAlignment.start:MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("Find",style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500
-                    ),),
-                    Text("Recipes",style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500
-                    ),)
-                  ],
-                ),
-                SizedBox(height: 30,),
-                Text("Want to have a recipe?",style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white
-                ),),
-                SizedBox(height: 8,),
-                Text("Just Enter the Ingredients you have and we will show you the best recipe for you.",style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white
-                  ),
-                ),
-                SizedBox(height: 30,),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Row(
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: !kIsWeb? Platform.isIOS? 60: 40: 40,horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: kIsWeb?MainAxisAlignment.start:MainAxisAlignment.center,
                     children: <Widget>[
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Enter Ingredients",
-                            hintStyle: TextStyle(
-                              fontSize: 18
-                            )
-                          ),
-                          style: TextStyle(
-                            fontSize: 18
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 16,),
-                      Container(
-                        child: Icon(Icons.search,color: Colors.white,),
-                      )
+                      Text("Find",style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500
+                      ),),
+                      Text("Recipes",style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500
+                      ),)
                     ],
                   ),
-                )
-              ],
+                  SizedBox(height: 30,),
+                  Text("Want to have a recipe?",style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontFamily: "Overpass"
+                  ),),
+                  SizedBox(height: 8,),
+                  Text("Just Enter the Ingredients you have and we will show you the best recipe for you.",style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontFamily: "OverpassRegular"
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            controller: textEditingController,
+                            decoration: InputDecoration(
+                              hintText: "Enter Ingredients",
+                              hintStyle: TextStyle(
+                                color: Colors.white.withOpacity(0.5),
+                                fontSize: 18,
+                                fontFamily: 'Overpass'
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 16,),
+                        InkWell(
+                          onTap: (){
+                            if(textEditingController.text.isNotEmpty){
+                              getRecipes(textEditingController.text);
+                              print("Just do it");
+                            }else{
+                              print("Don't do it");
+                            }
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              gradient: LinearGradient(
+                                colors: [
+                                  const Color(0xffA2834D),
+                                  const Color(0xffBC9A5F)
+                                ]
+                              )
+                            ),
+                            child: Icon(Icons.search,color: Colors.white,),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Container(
+                    child: GridView(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      physics: ClampingScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                        maxCrossAxisExtent: 200, mainAxisSpacing: 10
+                      ),
+                      children: List.generate(recipes.length, (index){
+                        return GridTile(
+                          child: RecipieTile(
+                            title: recipes[index].label,
+                            desc: recipes[index].source,
+                            imgUrl: recipes[index].image,
+                            url: recipes[index].url,
+                        )
+                      );
+                      }),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
@@ -92,3 +177,91 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+class RecipieTile extends StatefulWidget {
+  final String title, desc, imgUrl, url;
+
+  RecipieTile({this.title, this.desc, this.imgUrl, this.url});
+
+  @override
+  _RecipieTileState createState() => _RecipieTileState();
+}
+
+class _RecipieTileState extends State<RecipieTile> {
+  _launchURL(String url) async {
+    print(url);
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            if (kIsWeb) {
+              _launchURL(widget.url);
+            } else {
+              print(widget.url + " this is what we are going to see");
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => RecipeView(
+                        postUrl: widget.url,
+                      )));
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.all(8),
+            child: Stack(
+              children: <Widget>[
+                Image.network(
+                  widget.imgUrl,
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  width: 200,
+                  alignment: Alignment.bottomLeft,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.white30, Colors.white],
+                          begin: FractionalOffset.centerRight,
+                          end: FractionalOffset.centerLeft)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          widget.title,
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.black54,
+                              fontFamily: 'Overpass'),
+                        ),
+                        Text(
+                          widget.desc,
+                          style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.black54,
+                              fontFamily: 'OverpassRegular'),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
--- lib/views/recipe_view.dart
+++ lib/views/recipe_view.dart
@@ -1,13 +1,84 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class RecipeView extends StatefulWidget {
+
+  String postUrl;
+  RecipeView({this.postUrl});
+
   @override
   _RecipeViewState createState() => _RecipeViewState();
 }
 
 class _RecipeViewState extends State<RecipeView> {
+  String finalUrl;
+  final Completer<WebViewController> _controller=Completer<WebViewController>();
+  @override
+  void initState() {
+    // TODO: implement initState
+    if(widget.postUrl.contains("http://")){
+      finalUrl=widget.postUrl.replaceAll("http://", "https://");
+    }else{
+      finalUrl=widget.postUrl;
+    }
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                    top:  Platform.isIOS? 60: 40,right: 24, left: 24, bottom: 16),
+
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          const Color(0xff213A50),
+                          const Color(0xff071930)
+                        ]
+                    )
+                ),
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: kIsWeb?MainAxisAlignment.start:MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Find",style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500
+                    ),),
+                    Text("Recipes",style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500
+                    ),)
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height-100,
+                width: MediaQuery.of(context).size.width,
+                child: WebView(
+                  initialUrl: finalUrl,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (WebViewController webViewController){
+                    setState(() {
+                      _controller.complete(webViewController);
+                    });
+                  },
+                ),
+              )
+            ],
+          ),
+        )
+    );
   }
 }
